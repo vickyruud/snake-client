22d0c3ecea284605ab5982388cdcb9b188238971
vickyruud <aprajit.k@gmail.com>
2021-08-16 14:09:39 +0000

    adding play.js after first step

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+/**
+ * *establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('You ded cus you idled!')
+  })
+  return conn
+};
+
+console.log("Connecting...");
+connect();
