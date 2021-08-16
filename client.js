64d24ca0c6c9244d52fd6d028cbd503c8ad11242
vickyruud <aprajit.k@gmail.com>
2021-08-16 14:13:37 +0000

    adding client and modified play.

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  //interpret incoming data as text
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('You ded cus you idled!')
+  })
+  return conn
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,3 @@
-const net = require('net');
-
-/**
- * *establishes connection with the game server
- */
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  //interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log('You ded cus you idled!')
-  })
-  return conn
-};
-
+const connect = require('./client');
 console.log("Connecting...");
 connect();
